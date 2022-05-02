d64d07a6b9288397f7a3df8301a565ce8279d24a
Suprim Poudel <suprempoudel@gmail.com>
2022-05-02 09:46:16 +0545

    Added Navigation Controller Dependency

--- app/src/main/java/com/np/suprimpoudel/day3assignment/MainActivity.kt
+++ app/src/main/java/com/np/suprimpoudel/day3assignment/MainActivity.kt
@@ -2,10 +2,27 @@ package com.np.suprimpoudel.day3assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.np.suprimpoudel.day3assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Binding For MainActivity
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavigationController()
+    }
+
+    private fun setUpNavigationController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerMainActivity) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
